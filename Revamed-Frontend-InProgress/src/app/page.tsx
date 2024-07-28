cd8ee4bd5002180ea5018027e5610652093f5916
talha.rizwan <talha.rizwan7170@hotmail.com>
2024-07-28 21:52:52 +0500

    frontend changes

--- Revamed-Frontend-InProgress/src/app/page.tsx
+++ Revamed-Frontend-InProgress/src/app/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import ButtonPrimary from "@/shared/Button/ButtonPrimary";
 
 function PageHome() {
   return (
     <div className="nc-PageHome relative overflow-hidden">
-      <div className="container">
-        <div className="">
-          <h1 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-            Welcome to my website
-          </h1>
-        </div>
+      <div className="container flex flex-col justify-center items-center mx-auto">
+        <h1 className="my-20 text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100">
+          Welcome to my website
+        </h1>
+        <ButtonPrimary className="mx-auto mb-10" href="/login">
+          Get Started
+        </ButtonPrimary>
       </div>
     </div>
   );
